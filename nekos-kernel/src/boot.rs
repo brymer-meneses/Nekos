a60a02cc42578f089bef0bb8340214e199a9351d
brymer-meneses <68867487+brymer-meneses@users.noreply.github.com>
2025-10-20 15:21:10 +0800

    implement trap handler

--- nekos-kernel/src/boot.rs
+++ nekos-kernel/src/boot.rs
@@ -1,23 +1,54 @@
 #![no_std]
 #![no_main]
 
+mod exception;
 mod sbi;
+
+use core::{
+    arch::{self, asm},
+    ptr,
+};
 
 #[no_mangle]
 pub extern "C" fn kernel_main() -> ! {
-    sbi::load_screen();
-    loop {}
-}
+    // Zero the BSS section.
+    let bss_start = ptr::addr_of!(__bss_start);
+    let bss_end = ptr::addr_of!(__bss_end);
+    let bss_size = (bss_start as u64 - bss_end as u64) as usize;
+    unsafe {
+        ptr::write_bytes(ptr::addr_of_mut!(__bss_start), 0, bss_size);
+    };
 
-#[link_section = ".text.boot"]
-#[unsafe(naked)]
-#[no_mangle]
-unsafe extern "C" fn boot() -> ! {
-    core::arch::naked_asm!("la sp, __stack_top", "j kernel_main",);
+    println!("Hello world!");
+
+    unsafe {
+        asm!("csrw stvec, {}", in(reg) exception::handler as u64);
+    }
+
+    loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     println!("Panic at the kernel!");
     loop {}
+}
+
+extern "C" {
+    static __stack_top: u8;
+
+    static mut __bss_start: u8;
+    static __bss_end: u8;
+}
+
+#[link_section = ".text.boot"]
+#[unsafe(naked)]
+#[no_mangle]
+unsafe extern "C" fn boot() -> ! {
+    arch::naked_asm!(
+        "la sp, {stack_top}",
+        "j {kernel_main}",
+        stack_top = sym __stack_top,
+        kernel_main = sym kernel_main
+    );
 }
