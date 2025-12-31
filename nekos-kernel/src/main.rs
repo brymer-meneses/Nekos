d36166981d62a14835c8113786ff11229ba0ad75
brymer-meneses <68867487+brymer-meneses@users.noreply.github.com>
2025-12-31 08:14:36 +0800

    cleanup `kmain`

--- nekos-kernel/src/main.rs
+++ nekos-kernel/src/main.rs
@@ -10,34 +10,11 @@ mod mem;
 
 use arch::print;
 
-use crate::mem::VirtualAddr;
-
 #[unsafe(no_mangle)]
 extern "C" fn kmain() -> ! {
     boot::init();
     arch::init();
     mem::init();
-
-    let virtual_addr = VirtualAddr::new(0x001000);
-    let physical_addr = mem::allocate_pages(1).expect("Failed to allocate memory");
-    let boot_page_directory = boot::BOOT_PAGE_DIRECTORY
-        .get()
-        .expect("Boot not initialized");
-
-    let flags = mem::VirtualMemoryFlags::Writeable;
-
-    log::info!("Hello world!");
-
-    arch::map_page(boot_page_directory, virtual_addr, physical_addr, flags)
-        .expect("Failed to map page");
-
-    arch::flush_tlb();
-
-    let ptr = virtual_addr.as_ptr() as *mut u8;
-    unsafe {
-        *ptr = 10;
-        log::info!("{}", *ptr);
-    }
 
     arch::halt();
 }
